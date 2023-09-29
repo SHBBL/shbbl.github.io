bf276a8964dcb601650c157ee9c7c703b85b95bd
SHB <101410189+SHBBL@users.noreply.github.com>
2023-09-29 16:36:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,39 +5,71 @@ var tv = $('.tv');
 var __EVAL = (s) => eval(`void (__EVAL = ${__EVAL}); ${s}`);
 
 var term = $('#terminal').terminal(function(command, term) {
-    var cmd = $.terminal.parse_command(command);
-    if (cmd.name === 'exit') {
-        exit();
-    } else if (cmd.name === 'echo') {
-        term.echo(cmd.rest);
-    }else if (cmd.name === 'whoami') {
-        term.echo("SHB");
-    }else if (cmd.name === 'clear' || cmd.name === 'cls') {
-        clear();
-    }else if (cmd.name === 'ls' || cmd.name === 'dir') {
-        term.echo("index.html   script.js   style.css");
-    }else if (cmd.name === 'help') {
-        term.echo("echo [text]   whoami   clear/cls   ls/dir   help   setname [name] exit");
-    }else if (cmd.name === 'setname') { 
-        username = cmd.rest;
-        term.set_prompt('root@' + username + '> ');
-    }else if (cmd.name === 'getos') {
-        term.echo(getOS());
-    }else if (command !== '') {
-        try {
-            var result = __EVAL(command);
-            if (result && result instanceof $.fn.init) {
-                term.echo('<#jQuery>');
-            } else if (result && typeof result === 'object') {
-                tree(result);
-            } else if (result !== undefined) {
-                term.echo(new String(result));
+    var cmdln = $.terminal.parse_command(command);
+    var cmd = cmdln.args;
+    cmd.unshift(cmdln.name);
+    for (let i = 0; i < cmd.length; i++) {
+        if (cmd[i] != "&&"){
+            if (cmd[i] === 'exit') {
+                exit();
+            } else if (cmd[i] === 'echo') {
+                term.echo(cmd[i+1]);
+            }else if (cmd[i] === 'whoami') {
+                term.echo("SHB");
+            }else if (cmd[i] === 'clear' || cmd[i] === 'cls') {
+                clear();
+            }else if (cmd[i] === 'ls' || cmd[i] === 'dir') {
+                term.echo("index.html   script.js   style.css");
+            }else if (cmd[i] === 'help') {
+                help();
+            }else if (cmd[i] === 'setname') { 
+                username = cmd[i+1];
+                term.set_prompt('root@' + username + '> ');
+            }else if (cmd[i] === 'cd') {
+                    if (cmd[i+1].includes("https://")  || cmd[i+1].includes("http://")){
+                    window.location.replace(cmd[i+1]);
+                }else {
+                    window.location.replace('https://' + cmd[i+1]);
+                }
+            }else if (cmd[i] === 'cat') {
+                switch (cmd[i+1]) {
+                    case 'index.html':
+                        gettext('index.html');
+                        break;
+                    case 'script.js':
+                        gettext('script.js');
+                        break;
+                    case 'style.css':
+                        gettext('style.css');
+                        break;
+                    default:
+                        term.error('File not found');
+                        break;
+                }
             }
-        } catch(e) {
-            term.error(new String(e));
+            else if (command !== '') {
+                try {
+                    var result = __EVAL(command);
+                    if (result && result instanceof $.fn.init) {
+                        term.echo('<#jQuery>');
+                    } else if (result && typeof result === 'object') {
+                        tree(result);
+                    } else if (result !== undefined) {
+                        term.echo(new String(result));
+                    }
+                } catch(e){
+                    if (new String(e).includes("Syntax")){
+                        //term.echo("Type [[b;#fff;]help] to list commands");
+                    }
+                    else{
+                        term.error(new String(e));
+                    }
+                }
+            }
+            
         }
-
     }
+    //term.echo(n);
 }, {
     name: 'shell',
     onResize: set_size,
@@ -81,24 +113,25 @@ function exit() {
     }, 2000);
 }
 
-const getOS = () => {
-    const userAgent = window.navigator.userAgent;
-    const platform = window.navigator.platform;
-    const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
-    const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
-    const iosPlatforms = ['iPhone', 'iPad', 'iPod'];
-    
-    if (macosPlatforms.indexOf(platform) !== -1) {
-      return 'Mac OS';
-    } else if (iosPlatforms.indexOf(platform) !== -1) {
-      return 'iOS';
-    } else if (windowsPlatforms.indexOf(platform) !== -1) {
-      return 'Windows';
-    } else if (/Android/.test(userAgent)) {
-      return 'Android';
-    } else if (/Linux/.test(platform)) {
-      return 'Linux';
-    }
-    
-    return 'Unknown OS';
-  };
+function help() {
+    term.echo("Type [[b;#fff;]setname] [[b;#f00;]name] to set username");
+    term.echo("Type [[b;#fff;]cd] [[b;#f00;]site] to redirect to site");
+    term.echo("Type [[b;#fff;]whoami] to see author");
+    term.echo("Type [[b;#fff;]clear]/[[b;#fff;]cls] to clear terminal");
+    term.echo("Type [[b;#fff;]ls]/[[b;#fff;]dir] to list files"); 
+    term.echo("Type [[b;#fff;]help] to list commands");
+    term.echo("Type [[b;#fff;]exit] to exit site");
+}
+
+function gettext(file){
+    fetch(file)
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(data) {
+            term.echo(data);
+        })
+        .catch(function(error) {
+            term.error(error);
+        });
+}
